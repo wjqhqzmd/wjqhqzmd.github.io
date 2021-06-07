0477c1ba2ae1bd998252443cc1976c8f7e43e742
wjqhqzmd <wjqhqzmd@163.com>
2021-06-07 13:50:39 +0800

    Clear

--- pages/双色球_files/hm(7).js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?368fb7478442480b87000e634e2217ee";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
